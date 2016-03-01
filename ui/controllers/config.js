71003df806774411a7b794d04acd83e741df11a8
Wesley Rebelo <wrebe@agilesolutions.com>
2016-03-01 17:35:32 -0300

    tentando fazer o botao no email

--- ui/controllers/config.js
+++ ui/controllers/config.js
@@ -23,3 +23,24 @@ app.factory('Config', function() {
 		}
 	};
 });
+
+var obj = {
+ "nome": "wes",
+ "sobrenome": "Rebe",
+ "empresa": "agile",
+ "cargo": "forntend",
+ "email": "wesleyyw8@gmai",
+ "telefone": "123123",
+ "perguntas": {
+  "p1": "Eu influencio nas questões.",
+  "p2": ["SAP S/4 HANA", "Migração SAP ERP para S/4 Nuvem", "Solução Integral Agro (AgroBiz)", "GIS", "Outros"],
+  "p3": "Eu influencio nas questões."
+ }
+};
+$.ajax({url: "http://agilesolutions.com:8080/questionario/",
+	type: "POST",
+	data: obj,
+	success: function(result){
+  	alert('deu certo')  ;
+	}
+});
