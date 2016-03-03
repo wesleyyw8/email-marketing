7efd39d26c45f423c572ab565679d9b0ddd8295c
Wesley Rebelo <wrebe@agilesolutions.com>
2016-03-03 16:56:17 -0300

    triangulos adicionado na ending page

--- application.js
+++ application.js
@@ -27,7 +27,23 @@ app.factory('Config', function() {
 app.controller('EndController',
 ['$scope','$http','$routeParams','$location','Config', function($scope,$http,
 routeParams,location,Config){
-	
+	drawTriangles('.triangle.header', 250, 260, 270,0,40,0, "#3ea9de");
+	drawTriangles('.triangle.footer', 250, 270, 260,150,150,100, "#10172e");
+	function drawTriangles(className,x1, x2, x3,y1,y2,y3, color){
+		var canvas = angular.element(className+' canvas')[0];
+		if (canvas.getContext){
+		  var context = canvas.getContext('2d');
+		  context.beginPath();   
+		  context.moveTo(x1, y1); 
+		  context.lineTo(x2, y2);
+		  context.lineTo(x3, y3); 
+		  context.closePath();   
+		  context.fillStyle = color;
+		  context.strokeStyle = color;
+		  context.stroke();   
+		  context.fill();   
+		}
+	}
 }]);
 app.controller('FormularioController',
 ['$scope','$http','$routeParams','$location','Config','toaster','usSpinnerService' ,function($scope,$http,
--- controllers/end.js
+++ controllers/end.js
@@ -1,5 +1,21 @@
 app.controller('EndController',
 ['$scope','$http','$routeParams','$location','Config', function($scope,$http,
 routeParams,location,Config){
-	
+	drawTriangles('.triangle.header', 250, 260, 270,0,40,0, "#3ea9de");
+	drawTriangles('.triangle.footer', 250, 270, 260,150,150,100, "#10172e");
+	function drawTriangles(className,x1, x2, x3,y1,y2,y3, color){
+		var canvas = angular.element(className+' canvas')[0];
+		if (canvas.getContext){
+		  var context = canvas.getContext('2d');
+		  context.beginPath();   
+		  context.moveTo(x1, y1); 
+		  context.lineTo(x2, y2);
+		  context.lineTo(x3, y3); 
+		  context.closePath();   
+		  context.fillStyle = color;
+		  context.strokeStyle = color;
+		  context.stroke();   
+		  context.fill();   
+		}
+	}
 }]);
